de8cbb3acdace319f2874788153b159e4bd20c19
Byabasaija <basaijapascal9@gmail.com>
2022-02-21 03:17:06 +0300

    finish containerising components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { About, Footer, Header, Skills, Testimonials, Work } from './containers'
 
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
