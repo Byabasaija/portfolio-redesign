02aced36c776d6d4b933593c5d765cff10431919
Byabasaija <basaijapascal9@gmail.com>
2022-02-21 14:15:36 +0300

    add header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { About, Footer, Header, Skills, Testimonials, Work } from './containers'
 import './App.scss'
+import { Navbar } from './components';
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
