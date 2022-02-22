0e183ef5697fb8c48434076efa794b7c19d7cd77
Byabasaija <basaijapascal9@gmail.com>
2022-02-22 19:53:12 +0300

    fix a typo

--- src/containers/Work/Work.jsx
+++ src/containers/Work/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
 
   return (
     <>
-      <h2 className="head-text">My<span>Portfolio</span> Projects</h2>
+      <h2 className="head-text">My<span> Portfolio</span> Projects</h2>
 
       <div className="app__work-filter">
         {['UI/UX', 'Ruby on Rails', 'Js', 'React JS', 'All'].map((item, index) => (
